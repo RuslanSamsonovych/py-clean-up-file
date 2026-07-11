be3e8ce6e8371369a8c596dadeac087c61f1ddf6
Ruslan Samsonovych <arankutakepakuta@gmail.com>
2026-07-11 13:39:31 +0300

    implement Сlean Up File

--- app/main.py
+++ app/main.py
@@ -1,3 +1,24 @@
+import os
+from types import TracebackType
+
+
 class CleanUpFile:
-    # write your code here
-    pass
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+        self.mode = "w"
+        self.file = None
+
+    def __enter__(self) -> "CleanUpFile":
+        self.file = open(self.filename, self.mode)
+        return self
+
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None,
+    ) -> None:
+        if self.file:
+            self.file.close()
+        if os.path.exists(self.filename):
+            os.remove(self.filename)
